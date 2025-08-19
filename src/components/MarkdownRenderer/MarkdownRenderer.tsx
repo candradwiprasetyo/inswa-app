334645125029f134ccefbbb45d7c0d1a47948401
unknown <candradwiprasetyo@gmail.com>
2025-08-19 18:37:14 +0700

    feat: create login page

--- src/components/MarkdownRenderer/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -12,13 +12,9 @@ const MarkdownRenderer: React.FC<Props> = ({ content }) => {
     <div className="prose max-w-none">
       <ReactMarkdown
         components={{
-          ul: ({ node, ...props }) => (
-            <ul className="list-disc pl-6" {...props} />
-          ),
-          ol: ({ node, ...props }) => (
-            <ol className="list-decimal pl-6" {...props} />
-          ),
-          li: ({ node, ...props }) => <li className="mb-1" {...props} />,
+          ul: ({ ...props }) => <ul className="list-disc pl-6" {...props} />,
+          ol: ({ ...props }) => <ol className="list-decimal pl-6" {...props} />,
+          li: ({ ...props }) => <li className="mb-1" {...props} />,
         }}
       >
         {content.replace(/\u2028|\u2029|\u00A0/g, " ")}
