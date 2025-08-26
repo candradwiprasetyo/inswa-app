84e50c6b89b0cfbf34fc833edf3400bec2d5226e
unknown <candradwiprasetyo@gmail.com>
2025-08-26 13:55:33 +0700

    feat: remove unused component

--- src/features/program/detail/index.tsx
+++ src/features/program/detail/index.tsx
@@ -2,7 +2,6 @@
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
 import Content from "./Content";
 
 export default function ProgramDetail() {
