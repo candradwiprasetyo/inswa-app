3c6a29044ea353739a6ca298ab9e1976de78d58a
unknown <candradwiprasetyo@gmail.com>
2025-09-28 11:24:04 +0700

    feat: add embla carousel on mitra list

--- src/features/homepage/Member.tsx
+++ src/features/homepage/Member.tsx
@@ -4,34 +4,46 @@ import Image from "next/image";
 import usePublicPartner from "@/hooks/usePublicPartner";
 import { cdnLoader } from "@/lib/cdnLoader";
 import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
 
 export default function Member() {
-  const { partners, loading } = usePublicPartner("1");
-  const { partners: sertifikat } = usePublicPartner("2");
+  const { partners = [], loading } = usePublicPartner("1");
+  const { partners: sertifikat = [] } = usePublicPartner("2");
 
-  const [emblaRef] = useEmblaCarousel({
-    align: "start",
-    loop: false,
-    slidesToScroll: 1,
-  });
+  const [emblaRef] = useEmblaCarousel(
+    {
+      align: "start",
+      loop: true,
+      dragFree: true,
+    },
+    [
+      AutoScroll({
+        speed: 1,
+        stopOnMouseEnter: true,
+        stopOnInteraction: false,
+      }),
+    ]
+  );
 
   return (
     <div className="bg-gradient-to-b from-white bg-surface-success pb-24 xl:pb-32 relative">
       <div className="bg-line absolute inset-0 bg-[500%] md:bg-contain"></div>
+
       <div className="md:flex gap-10 mx-auto max-w-6xl px-4 md:px-10 pt-10 md:pt-16 pb-20 relative">
         <div className="flex-none hidden md:inline md:w-1/3 min-w-[300px]">
-          {sertifikat?.map((sertifikat) => (
+          {sertifikat.map((s) => (
             <Image
-              key={sertifikat.id}
+              key={s.id}
               loader={cdnLoader}
-              src={sertifikat.image}
-              alt={sertifikat.name}
+              src={s.image}
+              alt={s.name}
               width={400}
               height={331}
               className="w-full"
             />
           ))}
         </div>
+
         <div className="flex-grow">
           <div className="mb-6 font-pathway-extreme text-[32px] font-medium">
             InSWA Resmi Bagian Dari ISWA
@@ -43,32 +55,33 @@ export default function Member() {
             mengoptimal dan meningkatkan efisiensi pengelolaan sampah di level
             global.
           </div>
+
           <div className="md:hidden block mb-6">
-            {sertifikat?.map((sertifikat) => (
+            {sertifikat.map((s) => (
               <Image
-                key={sertifikat.id}
+                key={s.id}
                 loader={cdnLoader}
-                src={sertifikat.image}
-                alt={sertifikat.name}
+                src={s.image}
+                alt={s.name}
                 width={400}
                 height={331}
                 className="w-full"
               />
             ))}
           </div>
-          <div className="font-extrabold mb-4">Mitra Kami</div>
 
+          <div className="font-extrabold mb-4">Mitra Kami</div>
           {loading && <div>Loading...</div>}
 
           <div
-            className="embla md:mt-10 hidden md:block overflow-hidden"
             ref={emblaRef}
+            className="relative mx-auto w-full max-w-6xl overflow-hidden rounded-xl"
           >
             <div className="embla__container flex">
-              {partners?.map((partner) => (
+              {partners.map((partner) => (
                 <div
-                  className="embla__slide shrink-0 w-1/6 px-2"
                   key={partner.id}
+                  className="embla__slide shrink-0 w-1/6 px-3"
                 >
                   <div className="aspect-[3/2] border bg-white border-primary-light rounded-lg flex items-center px-2">
                     <Image
