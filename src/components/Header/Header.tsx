d0f20b4b6c1878e823852dde2365051348b8affc
unknown <candradwiprasetyo@gmail.com>
2025-12-09 07:44:26 +0700

    feat: update header mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,6 +26,7 @@ export default function Header({ variant = "default" }: HeaderProps) {
   const [isScrolled, setIsScrolled] = useState(false);
   const pathname = usePathname();
   const { user, logout } = useAuth();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -46,113 +47,120 @@ export default function Header({ variant = "default" }: HeaderProps) {
   }, []);
 
   return (
-    <div
-      className={clsx(
-        `transition-colors duration-500 min-h-[64px] max-w-7xl md:mx-auto pt-2 rounded-xl right-3 md:right-10 left-3 md:left-10 px-3 md:px-10 flex fixed z-50 top-3 items-start`,
-        variant === "default"
-          ? isScrolled
-            ? "bg-header-secondary"
-            : "bg-header-primary"
-          : "bg-header-white"
-      )}
-    >
-      <div className="flex-grow flex items-start gap-8">
-        <div className="flex-none">
-          <Link href="/">
-            <Image
-              src={
-                isScrolled || variant !== "default"
-                  ? "/assets/images/header-logo-white.png"
-                  : "/assets/images/header-logo.png"
-              }
-              width={97}
-              height={36}
-              alt="Header Logo"
-              className="w-28 md:w-32"
-            />
-          </Link>
-        </div>
-        <div className="flex-grow">
-          <div
-            className={clsx(
-              "gap-8 font-medium hidden lg:flex h-12 items-center",
-              isScrolled || variant !== "default"
-                ? "text-tertiary-light"
-                : "text-white"
-            )}
-          >
-            {navItems.map((item) => (
-              <Link
-                key={`item-${item.href}`}
-                href={item.href}
-                className={`transition-colors hover:text-action-hover text-sm ${
-                  pathname.startsWith(item.href) ? "text-green" : ""
-                }`}
-              >
-                {item.label}
-              </Link>
-            ))}
+    <>
+      <div
+        className={clsx(
+          `transition-colors duration-500 min-h-[64px] max-w-7xl md:mx-auto pt-2 rounded-xl right-3 md:right-10 left-3 md:left-10 px-3 md:px-10 flex fixed z-50 top-3 items-start`,
+          variant === "default"
+            ? isScrolled
+              ? "bg-header-secondary"
+              : "bg-header-primary"
+            : "bg-header-white"
+        )}
+      >
+        <div className="flex-grow flex items-start gap-8">
+          <div className="flex-none">
+            <Link href="/">
+              <Image
+                src={
+                  isScrolled || variant !== "default"
+                    ? "/assets/images/header-logo-white.png"
+                    : "/assets/images/header-logo.png"
+                }
+                width={97}
+                height={36}
+                alt="Header Logo"
+                className="w-28 md:w-32"
+              />
+            </Link>
           </div>
-          {pathname.startsWith("/tentang-kami") && isScrolled && (
+          <div className="flex-grow">
             <div
               className={clsx(
-                "flex gap-8 mt-4",
+                "gap-8 font-medium hidden lg:flex h-12 items-center",
                 isScrolled || variant !== "default"
                   ? "text-tertiary-light"
                   : "text-white"
               )}
             >
-              {tabData.map((tab, index) => (
-                <LinkScroll
-                  key={tab.id}
-                  to={tab.id}
-                  smooth={true}
-                  duration={500}
-                  offset={index === 0 ? -120 : -100}
-                  spy={true}
-                  className={`transition-colors hover:text-action-hover text-xs pb-2 cursor-pointer`}
-                  activeClass="text-green font-bold border-b-[3px] border-green"
+              {navItems.map((item) => (
+                <Link
+                  key={`item-${item.href}`}
+                  href={item.href}
+                  className={`transition-colors hover:text-action-hover text-sm ${
+                    pathname.startsWith(item.href) ? "text-green" : ""
+                  }`}
                 >
-                  {tab.label}
-                </LinkScroll>
+                  {item.label}
+                </Link>
               ))}
             </div>
-          )}
-        </div>
-      </div>
-
-      <div className="flex-none gap-2 hidden lg:flex pt-2">
-        {!user ? (
-          <div className="flex-none gap-2 hidden lg:flex">
-            <Button
-              title="Masuk"
-              href="/login"
-              customClass="h-8 text-sm border border-secondary-light text-action-hover"
-              variant="transparent"
-            />
-            <Button title="Daftar" customClass="h-8 text-sm" href="/daftar" />
-          </div>
-        ) : (
-          <div className="flex-none gap-4 hidden lg:flex items-center text-sm">
-            <Link href={"/dashboard"}>
-              <span
-                className={
+            {pathname.startsWith("/tentang-kami") && isScrolled && (
+              <div
+                className={clsx(
+                  "flex gap-8 mt-4",
                   isScrolled || variant !== "default"
                     ? "text-tertiary-light"
                     : "text-white"
-                }
+                )}
               >
-                Hi, {user.name}
-              </span>
-            </Link>
-            <Button title="Logout" customClass="h-8 text-sm" onClick={logout} />
+                {tabData.map((tab, index) => (
+                  <LinkScroll
+                    key={tab.id}
+                    to={tab.id}
+                    smooth={true}
+                    duration={500}
+                    offset={index === 0 ? -120 : -100}
+                    spy={true}
+                    className={`transition-colors hover:text-action-hover text-xs pb-2 cursor-pointer`}
+                    activeClass="text-green font-bold border-b-[3px] border-green"
+                  >
+                    {tab.label}
+                  </LinkScroll>
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </div>
 
-      <div className="flex-none lg:hidden inline">
-        <Link href={"/daftar"}>
-          <button className="h-9 px-4 bg-green-gradient font-semibold flex items-center justify-center rounded-tl-[24px] rounded-br-[24px] rounded-bl rounded-tr text-primary-light">
+        <div className="flex-none gap-2 hidden lg:flex pt-2">
+          {!user ? (
+            <div className="flex-none gap-2 hidden lg:flex">
+              <Button
+                title="Masuk"
+                href="/login"
+                customClass="h-8 text-sm border border-secondary-light text-action-hover"
+                variant="transparent"
+              />
+              <Button title="Daftar" customClass="h-8 text-sm" href="/daftar" />
+            </div>
+          ) : (
+            <div className="flex-none gap-4 hidden lg:flex items-center text-sm">
+              <Link href={"/dashboard"}>
+                <span
+                  className={
+                    isScrolled || variant !== "default"
+                      ? "text-tertiary-light"
+                      : "text-white"
+                  }
+                >
+                  Hi, {user.name}
+                </span>
+              </Link>
+              <Button
+                title="Logout"
+                customClass="h-8 text-sm"
+                onClick={logout}
+              />
+            </div>
+          )}
+        </div>
+
+        <div className="flex-none lg:hidden inline">
+          <button
+            onClick={() => setIsMobileMenuOpen(true)}
+            className="mt-[6px] h-9 px-4 bg-green-gradient font-semibold flex items-center justify-center rounded-tl-[24px] rounded-br-[24px] rounded-bl rounded-tr text-primary-light"
+          >
             <Image
               src="/assets/icons/mobile-menu.svg"
               alt="Mobile Menu"
@@ -160,8 +168,40 @@ export default function Header({ variant = "default" }: HeaderProps) {
               height={24}
             />
           </button>
-        </Link>
+        </div>
       </div>
-    </div>
+
+      <div
+        className={clsx(
+          "fixed inset-0 z-[999] bg-gradient-to-b from-white bg-surface-success text-primary-light font-pathway-extreme transition-transform duration-500",
+          isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
+        )}
+      >
+        <div className="bg-line absolute inset-0 bg-[500%] md:bg-contain pointer-events-none"></div>
+
+        <div
+          onClick={() => setIsMobileMenuOpen(false)}
+          className="absolute w-8 h-8 rounded-full bg-green-gradient top-5 right-5 text-white flex items-center justify-center cursor-pointer"
+        >
+          <span className="material-symbols-outlined">close</span>
+        </div>
+
+        <div className="flex flex-col items-center justify-center h-full space-y-10 text-3xl font-medium">
+          {navItems.map((item) => (
+            <Link
+              key={`item-${item.href}`}
+              href={item.href}
+              onClick={() => setIsMobileMenuOpen(false)} // close after click
+              className={clsx(
+                "transition-colors hover:text-action-hover",
+                pathname.startsWith(item.href) && "text-green"
+              )}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/MotionLayout/MotionLayout.tsx
+++ src/components/MotionLayout/MotionLayout.tsx
@@ -13,11 +13,11 @@ export default function MotionLayout({
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        key={pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.2, ease: "easeInOut" }}
+      // key={pathname}
+      // initial={{ opacity: 0 }}
+      // animate={{ opacity: 1 }}
+      // exit={{ opacity: 0 }}
+      // transition={{ duration: 0.2, ease: "easeInOut" }}
       >
         {children}
       </motion.div>
