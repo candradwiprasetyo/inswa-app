b26d197511934b6710f7a9c118a93d2c634b74a1
unknown <candradwiprasetyo@gmail.com>
2026-01-19 22:11:13 +0700

    feat: update postgres ssl

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,10 @@ const pool = new Pool({
   host: process.env.PG_HOST,
   database: process.env.PG_DB,
   port: Number(process.env.PG_PORT),
-  ssl: false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
